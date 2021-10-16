02970e9a8a8f4ffb850a88c06230d22778f1de2d
UdithaIshan <uditha.ishan34@gmail.com>
2021-10-16 22:43:37 +0530

    Project initialized

---
+++ src/main/java/com/assignment/petstore/PetstoreRestApplication.java
@@ -0,0 +1,11 @@
+package com.assignment.petstore;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class PetstoreRestApplication extends Application {
+}
